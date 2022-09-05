2b17acb0790576ebfa01787f9e966fb8791d3728
pampukhaleo <lgpampukha@gmail.com>
2022-09-05 17:19:40 +0300

    navigation next prev today logic

--- scripts/header/navigation.js
+++ scripts/header/navigation.js
@@ -2,20 +2,42 @@ import { getItem, setItem } from '../common/storage.js';
 import { renderWeek } from '../calendar/calendar.js';
 import { renderHeader } from '../calendar/header.js';
 import { getStartOfWeek, getDisplayedMonth } from '../common/time.utils.js';
+import shmoment from '../common/shmoment.js';
 
 const navElem = document.querySelector('.navigation');
-const displayedMonthElem = document.querySelector(
-  '.navigation__displayed-month'
-);
+const displayedMonthElem = document.querySelector('.navigation__displayed-month');
 
 function renderCurrentMonth() {
   // отрисовать месяц, к которому относиться текущая неделя (getDisplayedMonth)
   // вставить в .navigation__displayed-month
+  displayedMonthElem.innerHTML = getDisplayedMonth(getItem('displayedWeekStart'));
 }
 
-const onChangeWeek = (event) => {
+const onChangeWeek = event => {
   // при переключении недели обновите displayedWeekStart в storage
   // и перерисуйте все необходимые элементы страницы (renderHeader, renderWeek, renderCurrentMonth)
+  console.dir(event.target.dataset.direction);
+  const monday = getItem('displayedWeekStart');
+  console.log(monday);
+  switch (event.target.dataset.direction) {
+    case 'next':
+      setItem('displayedWeekStart', shmoment(monday).add('days', 7).result());
+      renderHeader();
+      renderCurrentMonth();
+      break;
+    case 'prev':
+      setItem('displayedWeekStart', shmoment(monday).subtract('days', 7).result());
+      renderHeader();
+      renderCurrentMonth();
+      break;
+    case 'today':
+      setItem('displayedWeekStart', getStartOfWeek(new Date()));
+      renderHeader();
+      renderCurrentMonth();
+      break;
+    default:
+      break;
+  }
 };
 
 export const initNavigation = () => {
